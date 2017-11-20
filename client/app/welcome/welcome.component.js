15951e76f7a720690f065144a1e4c5400cfabb33
Nikhil Bhandari <nikhil.bhandari@6point9.in>
2017-11-20 12:15:10 +0530

    Added queue manage

--- client/app/welcome/welcome.component.js
+++ client/app/welcome/welcome.component.js
@@ -1,5 +1,6 @@
 'use strict';
 const angular = require('angular');
+const _ = require('lodash');
 
 const uiRouter = require('angular-ui-router');
 
@@ -7,14 +8,17 @@ import routes from './welcome.routes';
 
 export class WelcomeComponent {
   /*@ngInject*/
-  constructor($http, $scope, socket) {
+  constructor($http, $scope, socket, $interval) {
     this.$http = $http;
     this.socket = socket;
+    this.$interval = $interval;
 
     $scope.$on('$destroy', function () {
       socket.unsyncUpdates('guest');
     });
   }
+
+  queue = [];
 
   $onInit() {
     this
@@ -22,18 +26,38 @@ export class WelcomeComponent {
       .get('/api/guests')
       .then((response) => {
         this.guests = response.data;
-        this.socket.syncUpdates('guest', this.guests);
-
+        this.socket.syncUpdates('guest', this.guests, (event, newGuest) => {
+          if (event === 'updated' && newGuest.lastTap) {
+            console.log('pushing', newGuest);
+            this.queue.push(newGuest);
+          }
+        });
       })
       .catch(() => {
 
       });
+
+    this
+      .$interval(() => {
+        let next = this.queue.shift();
+
+        if (next) {
+          this.current = next;
+        }
+
+      }, 5000);
   }
 
   getGuests() {
-    return (this.guests || []).filter((guest) => {
-      return guest.lastTap;
-    });
+    _
+      .chain(this.guests || [])
+      .filter((guest) => {
+        return guest.lastTap;
+      })
+      .sortBy((guest) => {
+        return guest.lastTap;
+      })
+      .value();
   }
 
 }
